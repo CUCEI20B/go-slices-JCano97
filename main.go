cdca2b2c3b03165f0428e0fc55661f3249501e31
Eduardo Cano <jose.cano6053@alumnos.udg.mx>
2020-10-10 14:19:29 -0500

    prueba01

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var entrada int
+	var suma int
+	//recibe entrero para definir tamaños del slice
+	fmt.Scan(&n)
+	s := make([]int, 0, n)
+	//recibe los enteros y los almacena en el slice
+	for i := 0; i < n; i++ {
+		fmt.Scan(&entrada)
+		s = append(s, entrada)
+	}
+	//realiza la suma
+	for _, v := range s {
+		suma += v
+	}
+	//muestra la suma
+	fmt.Println(suma)
 }
